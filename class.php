e5b5d285333181bb710b8d87da0b3feb99b2bba4
Ridha Maulana <ado@Ridhas-MacBook-Pro.local>
2019-04-01 15:59:57 +0700

    add connection db

--- class.php
+++ class.php
@@ -1,8 +1,27 @@
 <?php
 
-class jobogor(){
-    echo "dssdf";
+class Jobogor{
 
-}
+    var $servername = "localhost";
+    var $username = "username";
+    var $password = "password";
+    var $dbname = "myDBPDO";
+    var $conn;
+    function __construct($table){
+        try {
+            $this -> conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $this ->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e) {
+            echo   "<br>" . $e->getMessage();
+            }
+    }
+
+    
+
+
+
+};
 
 ?>
