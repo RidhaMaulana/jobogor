b6b34110f9ca9a2671459d1eb8cb54ed6ecd3403
Ridha Maulana <ado@Ridhas-MacBook-Pro.local>
2019-04-02 11:27:29 +0700

    add insert func

--- class.php
+++ class.php
@@ -7,15 +7,24 @@ class Jobogor{
     var $password = "password";
     var $dbname = "myDBPDO";
     var $conn;
+
     function __construct($table){
         try {
-            $this -> conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $this ->conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             // set the PDO error mode to exception
             $this ->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(PDOException $e) {
             echo   "<br>" . $e->getMessage();
             }
+    }
+    function insert($field,$values){
+
+        $sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')";
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        echo "New record created successfully";
+  
     }
 
     
